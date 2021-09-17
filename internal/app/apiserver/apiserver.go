8d2f341891ff223a4c5fb2d8ab1ad034075bde0a
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2021-09-17 22:40:51 +0600

    added start functionality

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,11 +1,56 @@
 package apiserver
 
-type APIServer struct{}
+import (
+	"io"
+	"net/http"
 
-func New() *APIServer  {
-	return &APIServer{}
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
+type APIServer struct {
+	config *Config
+	logger *logrus.Logger
+	router *mux.Router
 }
 
+func New(config *Config) *APIServer {
+	return &APIServer{
+		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
+	}
+}
+
+// Start 
 func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.condfigureRouter()
+	s.logger.Info("Starting api server")
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
 	return nil
+}
+
+func (s *APIServer) condfigureRouter()  {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "hello")
+	}
 }
