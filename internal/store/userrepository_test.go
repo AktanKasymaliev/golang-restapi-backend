7af7df5a265c548d3f761c1e16f625ebe8c9aa32
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2021-09-19 14:57:17 +0600

    added test functionality on database

---
+++ internal/store/userrepository_test.go
@@ -0,0 +1,36 @@
+package store_test
+
+import (
+	"auth/internal/app/model"
+	"auth/internal/store"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserRepository_Create(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	u, err := s.User().Create(&model.User{
+		Email: "admin@admin.com",
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	email := "23admin@admin.com"
+	_, err := s.User().FindByEmail(email)
+	assert.Error(t, err)
+
+	s.User().Create(&model.User{
+		Email: "admin@admin.com",
+	})
+	u, err := s.User().FindByEmail(email)
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
